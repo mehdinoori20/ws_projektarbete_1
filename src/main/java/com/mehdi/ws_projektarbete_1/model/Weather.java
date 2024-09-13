424492be3dd478908f24fb8cb4abb8faa7892e18
mehdinoori20 <mehdi.noori1994@hotmail.com>
2024-09-13 16:33:29 +0200

    getter and setter

--- src/main/java/com/mehdi/ws_projektarbete_1/model/Weather.java
+++ src/main/java/com/mehdi/ws_projektarbete_1/model/Weather.java
@@ -18,4 +18,52 @@ public class Weather {
     private int humidity;
     private String weatherDescription;
     private LocalDateTime timestamp;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getWeatherDescription() {
+        return weatherDescription;
+    }
+
+    public void setWeatherDescription(String weatherDescription) {
+        this.weatherDescription = weatherDescription;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
 }
