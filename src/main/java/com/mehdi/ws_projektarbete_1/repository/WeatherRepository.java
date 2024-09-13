1599b4d12d4d399f7f06b92b926b1b0708432922
mehdinoori20 <mehdi.noori1994@hotmail.com>
2024-09-13 16:29:14 +0200

    Add WeatherRepository interface for database operations

--- src/main/java/com/mehdi/ws_projektarbete_1/repository/WeatherRepository.java
+++ src/main/java/com/mehdi/ws_projektarbete_1/repository/WeatherRepository.java
@@ -1,4 +1,9 @@
 package com.mehdi.ws_projektarbete_1.repository;
 
-public interface WeatherRepository {
+import com.mehdi.ws_projektarbete_1.model.Weather;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WeatherRepository extends JpaRepository<Weather, Long> {
 }
