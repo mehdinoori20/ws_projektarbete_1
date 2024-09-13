c1a24478992247713abf340e175e86253345095e
mehdinoori20 <mehdi.noori1994@hotmail.com>
2024-09-13 15:26:11 +0200

    Initial commit with basic project structure

---
+++ src/main/java/com/mehdi/ws_projektarbete_1/WsProjektarbete1Application.java
@@ -0,0 +1,13 @@
+package com.mehdi.ws_projektarbete_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsProjektarbete1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsProjektarbete1Application.class, args);
+	}
+
+}
