60031ab4be1bcfe6aa3aff40972f52edebdb07ec
mehdinoori20 <mehdi.noori1994@hotmail.com>
2024-09-13 16:25:13 +0200

    Add Weather entity class with basic fields

--- src/main/java/com/mehdi/ws_projektarbete_1/controller/WeatherController.java
+++ src/main/java/com/mehdi/ws_projektarbete_1/controller/WeatherController.java
@@ -1,4 +1,16 @@
 package com.mehdi.ws_projektarbete_1.controller;
 
+import com.mehdi.ws_projektarbete_1.service.WeatherService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/weather")
 public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
 }
--- src/main/java/com/mehdi/ws_projektarbete_1/model/Weather.java
+++ src/main/java/com/mehdi/ws_projektarbete_1/model/Weather.java
@@ -1,4 +1,21 @@
 package com.mehdi.ws_projektarbete_1.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Weather {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cityName;
+    private double temperature;
+    private int humidity;
+    private String weatherDescription;
+    private LocalDateTime timestamp;
 }
