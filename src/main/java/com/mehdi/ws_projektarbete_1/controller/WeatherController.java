aaca87ef8da73a508b44e81ccea6ac1ed426b476
mehdinoori20 <mehdi.noori1994@hotmail.com>
2024-09-24 17:06:26 +0200

    add id, citynme, temperature and humidity in weather class

--- src/main/java/com/mehdi/ws_projektarbete_1/controller/WeatherController.java
+++ src/main/java/com/mehdi/ws_projektarbete_1/controller/WeatherController.java
@@ -1,12 +1,16 @@
 package com.mehdi.ws_projektarbete_1.controller;
 
 
+import com.mehdi.ws_projektarbete_1.model.Weather;
+import com.mehdi.ws_projektarbete_1.service.WeatherService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/v1/weather")
 public class WeatherController {
+
 
 
 }
