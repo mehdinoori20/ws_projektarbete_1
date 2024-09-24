a102ed36da026a09b66cef9181d421e9a0c5052b
mehdinoori20 <mehdi.noori1994@hotmail.com>
2024-09-24 17:48:27 +0200

    Add basic security configuration

--- src/main/java/com/mehdi/ws_projektarbete_1/config/SecurityConfig.java
+++ src/main/java/com/mehdi/ws_projektarbete_1/config/SecurityConfig.java
@@ -1,11 +1,42 @@
 package com.mehdi.ws_projektarbete_1.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @Configuration
 public class  SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())  // Inaktivera CSRF om det behövs
+                .authorizeHttpRequests(authz -> authz
+                        .anyRequest().authenticated()  // Autentisera alla requests
+                )
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+   /* @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("admin123")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }*/
 
 
 }
